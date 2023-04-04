5c300d518cca5470fc26bcb020c91e07f526085a
WASSIM BOUHDID <HE201893@students.ephec.be>
2023-04-05 00:57:57 +0200

    table

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,349 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
-<template>
-  <main>
-    <TheWelcome />
-  </main>
+<template lang="">
+  <div>
+    <table>
+      <thead>
+        <tr>
+          <th colspan="1">Nom & prénom</th>
+          <th colspan="2">Lundi</th>
+          <th colspan="2">Mardi</th>
+          <th colspan="2">Mercredi</th>
+          <th colspan="2">Jeudi</th>
+          <th colspan="2">Vendredi</th>
+        </tr>
+        <tr>
+          <th colspan="1"></th>
+          <th colspan="1">AM</th>
+          <th colspan="1">PM</th>
+          <th colspan="1">AM</th>
+          <th colspan="1">PM</th>
+          <th colspan="1">AM</th>
+          <th colspan="1">PM</th>
+          <th colspan="1">AM</th>
+          <th colspan="1">PM</th>
+          <th colspan="1">AM</th>
+          <th colspan="1">PM</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>nom 1</td>
+          <td class="lundiAm"></td>
+          <td class="lundiPm"></td>
+          <td class="mardiAm"></td>
+          <td class="mardiPm"></td>
+          <td class="mercrediAm"></td>
+          <td class="mercrediPm"></td>
+          <td class="jeudiAm"></td>
+          <td class="jeudiPm"></td>
+          <td class="vendrediAm"></td>
+          <td class="vendrediPm"></td>
+        </tr>
+        <tr>
+          <td>nom 2</td>
+          <td class="lundiAm"></td>
+          <td class="lundiPm"></td>
+          <td class="mardiAm"></td>
+          <td class="mardiPm"></td>
+          <td class="mercrediAm"></td>
+          <td class="mercrediPm"></td>
+          <td class="jeudiAm"></td>
+          <td class="jeudiPm"></td>
+          <td class="vendrediAm"></td>
+          <td class="vendrediPm"></td>
+        </tr>
+        <tr>
+          <td>nom 3</td>
+          <td class="lundiAm"></td>
+          <td class="lundiPm"></td>
+          <td class="mardiAm"></td>
+          <td class="mardiPm"></td>
+          <td class="mercrediAm"></td>
+          <td class="mercrediPm"></td>
+          <td class="jeudiAm"></td>
+          <td class="jeudiPm"></td>
+          <td class="vendrediAm"></td>
+          <td class="vendrediPm"></td>
+        </tr>
+        <tr>
+          <td>nom 4</td>
+          <td class="lundiAm"></td>
+          <td class="lundiPm"></td>
+          <td class="mardiAm"></td>
+          <td class="mardiPm"></td>
+          <td class="mercrediAm"></td>
+          <td class="mercrediPm"></td>
+          <td class="jeudiAm"></td>
+          <td class="jeudiPm"></td>
+          <td class="vendrediAm"></td>
+          <td class="vendrediPm"></td>
+        </tr>
+        <tr>
+          <td>nom 5</td>
+          <td class="lundiAm"></td>
+          <td class="lundiPm"></td>
+          <td class="mardiAm"></td>
+          <td class="mardiPm"></td>
+          <td class="mercrediAm"></td>
+          <td class="mercrediPm"></td>
+          <td class="jeudiAm"></td>
+          <td class="jeudiPm"></td>
+          <td class="vendrediAm"></td>
+          <td class="vendrediPm"></td>
+        </tr>
+        <tr>
+          <td>nom 6</td>
+          <td class="lundiAm"></td>
+          <td class="lundiPm"></td>
+          <td class="mardiAm"></td>
+          <td class="mardiPm"></td>
+          <td class="mercrediAm"></td>
+          <td class="mercrediPm"></td>
+          <td class="jeudiAm"></td>
+          <td class="jeudiPm"></td>
+          <td class="vendrediAm"></td>
+          <td class="vendrediPm"></td>
+        </tr>
+        <tr>
+          <td>nom 7</td>
+          <td class="lundiAm"></td>
+          <td class="lundiPm"></td>
+          <td class="mardiAm"></td>
+          <td class="mardiPm"></td>
+          <td class="mercrediAm"></td>
+          <td class="mercrediPm"></td>
+          <td class="jeudiAm"></td>
+          <td class="jeudiPm"></td>
+          <td class="vendrediAm"></td>
+          <td class="vendrediPm"></td>
+        </tr>
+        <tr>
+          <td>nom 8</td>
+          <td class="lundiAm"></td>
+          <td class="lundiPm"></td>
+          <td class="mardiAm"></td>
+          <td class="mardiPm"></td>
+          <td class="mercrediAm"></td>
+          <td class="mercrediPm"></td>
+          <td class="jeudiAm"></td>
+          <td class="jeudiPm"></td>
+          <td class="vendrediAm"></td>
+          <td class="vendrediPm"></td>
+        </tr>
+        <tr>
+          <td>nom 9</td>
+          <td class="lundiAm"></td>
+          <td class="lundiPm"></td>
+          <td class="mardiAm"></td>
+          <td class="mardiPm"></td>
+          <td class="mercrediAm"></td>
+          <td class="mercrediPm"></td>
+          <td class="jeudiAm"></td>
+          <td class="jeudiPm"></td>
+          <td class="vendrediAm"></td>
+          <td class="vendrediPm"></td>
+        </tr>
+        <tr>
+          <td>nom 10</td>
+          <td class="lundiAm"></td>
+          <td class="lundiPm"></td>
+          <td class="mardiAm"></td>
+          <td class="mardiPm"></td>
+          <td class="mercrediAm"></td>
+          <td class="mercrediPm"></td>
+          <td class="jeudiAm"></td>
+          <td class="jeudiPm"></td>
+          <td class="vendrediAm"></td>
+          <td class="vendrediPm"></td>
+        </tr>
+        <tr>
+          <td>nom 11</td>
+          <td class="lundiAm"></td>
+          <td class="lundiPm"></td>
+          <td class="mardiAm"></td>
+          <td class="mardiPm"></td>
+          <td class="mercrediAm"></td>
+          <td class="mercrediPm"></td>
+          <td class="jeudiAm"></td>
+          <td class="jeudiPm"></td>
+          <td class="vendrediAm"></td>
+          <td class="vendrediPm"></td>
+        </tr>
+        <tr>
+          <td>nom 12</td>
+          <td class="lundiAm"></td>
+          <td class="lundiPm"></td>
+          <td class="mardiAm"></td>
+          <td class="mardiPm"></td>
+          <td class="mercrediAm"></td>
+          <td class="mercrediPm"></td>
+          <td class="jeudiAm"></td>
+          <td class="jeudiPm"></td>
+          <td class="vendrediAm"></td>
+          <td class="vendrediPm"></td>
+        </tr>
+        <tr>
+          <td>nom 13</td>
+          <td class="lundiAm"></td>
+          <td class="lundiPm"></td>
+          <td class="mardiAm"></td>
+          <td class="mardiPm"></td>
+          <td class="mercrediAm"></td>
+          <td class="mercrediPm"></td>
+          <td class="jeudiAm"></td>
+          <td class="jeudiPm"></td>
+          <td class="vendrediAm"></td>
+          <td class="vendrediPm"></td>
+        </tr>
+        <tr>
+          <td>nom 14</td>
+          <td class="lundiAm"></td>
+          <td class="lundiPm"></td>
+          <td class="mardiAm"></td>
+          <td class="mardiPm"></td>
+          <td class="mercrediAm"></td>
+          <td class="mercrediPm"></td>
+          <td class="jeudiAm"></td>
+          <td class="jeudiPm"></td>
+          <td class="vendrediAm"></td>
+          <td class="vendrediPm"></td>
+        </tr>
+        
+      </tbody>
+    </table>
+    <button> généré un planning aléatoire</button>
+    <button @click="generatePlanning()">télécharger le tableau</button>
+  </div>
 </template>
+<script>
+  export default {
+    name: 'HomeView',
+    components: {
+
+    },
+    data() {
+      return {
+        taches: {
+          "guichet": {
+            jour: 'tous',
+            nbrAm: 2,
+            nbPm: 2
+          },
+          "chimio": {
+            jour: 'tous',
+            nbrAm: 2
+          },
+          "prep. Mag.": {
+            jour: 'tous',
+            nbrPm: 1
+          },
+          "TPN Néon.": {
+            jour: ['lundi', 'mardi', 'jeudi', 'vendredi'],
+            nbrPm: 2
+          },
+          "Chariots": {
+            jour: 'tous',
+            nbrAm: 1
+          },
+          "USI": {
+            jour: 'tous',
+            nbrPm: 2
+          },
+          "Stups": {
+            jour: 'mercredi',
+            nbrAm: 2
+          },
+          "Urgences": {
+            jour: 'tous',
+            nbrPm: 1
+          },
+          "Ophtalmo": {
+
+          },
+          "Colis": {
+            jour: 'tous',
+            nbrPm: 1
+          },
+          "Q.Op.": {
+            jour: ['lundi', 'jeudi'],
+            nbrPm: 2
+          },
+          "satellites": {
+
+          },
+          "Encodage": {
+            jour: 'tous',
+            nbrAm: 1
+          }
+        },
+        planningGrid: [
+          [],
+          [],
+          [],
+          [],
+          [],
+          [],
+          [],
+          [],
+          [],
+          [],
+          [],
+          [],
+          []
+        ],
+        planningGridVersionTwo: {
+          nom1: [],
+          nom2: [],
+          nom3: [],
+          nom4: [],
+          nom5: [],
+          nom6: [],
+          nom7: [],
+          nom8: [],
+          nom9: [],
+          nom10: [],
+          nom11: [],
+          nom12: [],
+          nom13: []
+        }
+
+      }
+    },
+    methods: {
+      generatePlanning() {
+
+        for (let i in this.taches) {
+          for (let x in this.taches[i]) {
+            if (x == "jour") {
+              if (this.taches[i][x] == "tous") {
+                console.log(this.taches[i][x])
+              }else{
+                console.log(this.taches[i][x])
+              }
+            }
+            if (x == "nbrAm") {
+              
+            }
+            if (x == "nbrPm") {
+              
+            }
+          }
+        }
+        /** 
+         * (Math.random()*10).toFixed()
+          for ( let i in this.planningGridVersionTwo){
+
+        console.log(this.planningGridVersionTwo[i])
+
+          }
+          */
+      }
+    },
+    computed: {
+
+    }
+
+  }
+</script>
+<style lang="">
+
+</style>
