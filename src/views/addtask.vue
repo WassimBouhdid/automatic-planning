d439eaf8cd9e8b70282cfdb7cbc3f148bf23bdbf
Bouhdid Wassim <64103126+WassimBouhdid@users.noreply.github.com>
2023-04-26 23:34:57 +0200

    add task function and component

--- src/views/addtask.vue
+++ src/views/addtask.vue
@@ -3,52 +3,61 @@
         <div class="card-body">
             <div style="display:flex;justify-content: space-between;">
                 <h5 class="card-title"> {{ test2 }} </h5>
-                <button type="button" class="btn btn-danger" @click="test(this.test2)"
+                <button type="button" class="btn btn-danger" @click="deleteTask(this.test2)"
                     style="margin-left:0px !important;margin-right:0px !important;margin-top:0px !important;">X</button>
 
             </div>
                 <div>
                     <label>test1</label>
-                    <input type="text">
+                    <input type="text" v-model="addTaskForm2.nom">
                 </div>
                 <div>
-                    <input type="checkbox" id="scales" name="scales" checked>
-                    <label for="scales">lunid</label>
-                    <input type="checkbox" id="scales" name="scales">
+                    <input type="checkbox" id="scales" name="scales" checked v-model="addTaskForm2.jour1.lundi">
+                    <label for="scales">lundi</label>
+                    <input type="checkbox" id="scales" name="scales" v-model="addTaskForm2.jour1.mardi">
                     <label for="scales">mardi</label>
-                    <input type="checkbox" id="scales" name="scales">
+                    <input type="checkbox" id="scales" name="scales" v-model="addTaskForm2.jour1.mercredi">
                     <label for="scales">mercredi</label>
-                    <input type="checkbox" id="scales" name="scales">
+                    <input type="checkbox" id="scales" name="scales" v-model="addTaskForm2.jour1.jeudi">
                     <label for="scales">jeudi</label>
-                    <input type="checkbox" id="scales" name="scales">
+                    <input type="checkbox" id="scales" name="scales" v-model="addTaskForm2.jour1.vendredi">
                     <label for="scales">vendredi</label>
                 </div>
                 <div>
-                    <label>test1</label>
-                    <input type="number">
+                    <label>nbr AM</label>
+                    <input type="number" v-model="addTaskForm2.nbrAm">
                 </div>
+               
                 <div>
-                    <input type="checkbox" id="scales" name="scales" checked>
-                    <label for="scales">AM</label>
-                    <input type="checkbox" id="scales" name="scales">
-                    <label for="scales">PM</label>
+                    <label>nbr PM</label>
+                    <input type="number" v-model="addTaskForm2.nbrPm">
                 </div>
 
                 <div>
                     <label for="pet-select">Choose a pet:</label>
 
-                    <select id="pet-select">
+                    <select id="pet-select" multiple size="7" v-model="addTaskForm2.employes">
                         <option value="">--Please choose an option--</option>
-                        <option value="dog">Dog</option>
-                        <option value="cat">Cat</option>
-                        <option value="hamster">Hamster</option>
-                        <option value="parrot">Parrot</option>
-                        <option value="spider">Spider</option>
-                        <option value="goldfish">Goldfish</option>
+                        <option value="tous">Tout le monde</option>
+                        <option value="nom1">nom1</option>
+                        <option value="nom2">nom2</option>
+                        <option value="nom3">nom3</option>
+                        <option value="nom4">nom4</option>
+                        <option value="nom5">nom5</option>
+                        <option value="nom6">nom6</option>
+                        <option value="nom7">nom7</option>
+                        <option value="nom8">nom8</option>
+                        <option value="nom9">nom9</option>
+                        <option value="nom10">nom10</option>
+                        <option value="nom11">nom11</option>
+                        <option value="nom12">nom12</option>
+                        <option value="nom13">nom13</option>
+                        <option value="nom14">nom14</option>
                     </select>
                 </div>
 
         </div>
+        <button v-on:click="submitTasks()">submit task</button>
     </div>
 </template>
 <script>
@@ -58,14 +67,27 @@ export default {
     components: {
       
     },
-    data() {},
+    data() {
+        return{
+            addTaskForm2:{
+            nom: "",
+            jour1: {
+                lundi:'',
+                mardi:'',
+                mercredi:'',
+                jeudi:'',
+                vendredi:''
+            },
+            nbrAm: 0,
+            nbrPm: 0,
+            employes: ""
+        }
+        }
+    },
     methods: {
-    test(palu){
-        console.log(palu)
-        document.getElementById("task"+palu).remove()
-      },
-      deleteTask(){
-        document.getElementById("task1").remove()
+      
+      submitTasks(){
+        this.$emit('eventname', this.addTaskForm2)
       },
     }
 }
---
+++ src/views/rules.vue
@@ -0,0 +1,21 @@
+<template lang="">
+    <div>
+test
+    </div>
+</template>
+<script>
+export default {
+    name: 'addTask',
+    props:[] ,
+    components: {
+      
+    },
+    data() {},
+    methods: {
+     
+    }
+}
+</script>
+<style lang="">
+    
+</style>
