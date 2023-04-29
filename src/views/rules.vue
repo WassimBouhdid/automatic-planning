265f0a77ed3ed2dfe4f37d5bf2437c2f4867acf0
Bouhdid Wassim <64103126+WassimBouhdid@users.noreply.github.com>
2023-04-30 01:13:00 +0200

    colors + polyvalent table creation

--- src/views/rules.vue
+++ src/views/rules.vue
@@ -4,16 +4,31 @@
       <div class="card-body">
         <div style="display:flex;justify-content: space-between;">
           <h5 class="card-title">{{ item.nom }}</h5>
-          <button type="button" class="btn btn-danger" @click=""
-            style="margin-left:0px !important;margin-right:0px !important;margin-top:0px !important;">X</button>
+          <button type="button" class="btn btn-danger"
+            style="margin-left:0px !important;margin-right:0px !important;margin-top:0px !important;" v-bind:click="deleteRule()">X</button>
         </div>
         <div v-for="(value, key, index) in item.jour">
-          <h6 class="card-subtitle mb-2 text-muted">{{ key }} --- {{ value }}</h6>
+          <h5 class="card-subtitle mb-2 text-muted">{{ key }}</h5>
+          <div class="row">
+            <div class="col">
+              <h6> nbrAm </h6>
+              <p>{{ value.nbrAm }}</p>
+            </div>
+            <div class="col">
+              <h6>nbrPm</h6>
+              <p>{{ value.nbrPm }}</p>
+            </div>
+          </div>
+          <hr>
         </div>
         <h5> les employés selectionnés</h5>
         <div v-for="(value, key, index) in item.employes">
-          
+
           <p class="card-text"> {{ value }}</p>
+        </div>
+        <h5> couleur </h5>
+        <div v-bind:style="{'background-color': item.color}">
+          {{ item.color }}
         </div>
       </div>
     </div>
@@ -28,7 +43,9 @@ export default {
     },
     data() {},
     methods: {
-     
+      deleteRule(){
+        this.$emit('eventname', this.addTaskForm2)
+      }
     }
 }
 </script>
