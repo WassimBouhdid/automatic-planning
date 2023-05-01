09e2c18b8b72d1b59730e625e1b5c235eab997a1
Bouhdid Wassim <64103126+WassimBouhdid@users.noreply.github.com>
2023-05-02 00:55:55 +0200

    polyvalence du planning terminé

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@ import { RouterLink, RouterView } from 'vue-router'
   <header>
     <nav class="navbar fixed-top navbar-expand-lg navbar-light bg-light">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">
+    <a class="navbar-brand" href="/">
       <img src="./assets/pngwing.com.png" alt="" width="45" height="32" class="d-inline-block align-text-top">
       Organizer
     </a>
--- src/views/addtask.vue
+++ src/views/addtask.vue
@@ -1,125 +1,130 @@
 <template lang="">
-    <div class="card" style="width: 35rem; margin-right: auto;margin-left: auto;">
-        <div class="card-body">
-            <div style="display:flex;justify-content: space-between;">
-                <h5 class="card-title"> créer une tâches </h5>
-                <button type="button" class="btn btn-danger" @click="deleteForm()"
-                    style="margin-left:0px !important;margin-right:0px !important;margin-top:0px !important;">X</button>
+    <from>
+        <div class="card" style="width: 35rem; margin-right: auto;margin-left: auto;">
+            <div class="card-body">
+                <div style="display:flex;justify-content: space-between;">
+                    <h5 class="card-title"> créer une tâches </h5>
+                    <button type="button" class="btn btn-danger" @click="deleteForm()"
+                        style="margin-left:0px !important;margin-right:0px !important;margin-top:0px !important;">X</button>
 
-            </div>
-            <div>
-                <label>nom de la tâche</label>
-                <input type="text" v-model="addTaskForm2.nom">
-            </div>
-            <div class="row">
-                <div class="col">
-                    <div style="display:block;">
-                        <input type="checkbox" id="scales" name="scales" checked
-                            v-model="addTaskForm2.jour1.lundi.checked">
-                        <label for="scales">lundi</label>
-                        <div v-if="addTaskForm2.jour1.lundi.checked == true">
-                            <div>
-                                <label> lundi nombre AM </label>
-                                <input min=0 type="number" v-model="addTaskForm2.jour1.lundi.nbrAm">
+                </div>
+                <div>
+                    <label>nom de la tâche : </label>
+                    <input type="text" minlength="0" maxlength="8" placeholder=" nom de la tâche "
+                        v-model="addTaskForm2.nom" >
+                </div>
+                <div class="row">
+                    <div class="col">
+                        <div style="display:block;">
+                            <input type="checkbox" id="scales" name="scales" checked
+                                v-model="addTaskForm2.jour1.lundi.checked">
+                            <label for="scales">lundi</label>
+                            <div v-if="addTaskForm2.jour1.lundi.checked == true">
+                                <div>
+                                    <label> lundi nombre AM </label>
+                                    <input min=0 type="number" v-model="addTaskForm2.jour1.lundi.nbrAm">
+                                </div>
+
+                                <div>
+                                    <label> lundi nombre PM </label>
+                                    <input min=0 type="number" v-model="addTaskForm2.jour1.lundi.nbrPm">
+                                </div>
                             </div>
+                        </div>
+                        <div style="display:block;">
+                            <input type="checkbox" id="scales" name="scales" v-model="addTaskForm2.jour1.mardi.checked">
+                            <label for="scales">mardi</label>
+                            <div v-if="addTaskForm2.jour1.mardi.checked == true">
+                                <div>
+                                    <label>mardi nombre AM </label>
+                                    <input min=0 type="number" v-model="addTaskForm2.jour1.mardi.nbrAm">
+                                </div>
 
-                            <div>
-                                <label> lundi nombre PM </label>
-                                <input min=0 type="number" v-model="addTaskForm2.jour1.lundi.nbrPm">
+                                <div>
+                                    <label>mardi nombre PM </label>
+                                    <input min=0 type="number" v-model="addTaskForm2.jour1.mardi.nbrPm">
+                                </div>
+                            </div>
+                        </div>
+                        <div style="display:block;">
+                            <input type="checkbox" id="scales" name="scales"
+                                v-model="addTaskForm2.jour1.mercredi.checked">
+                            <label for="scales">mercredi</label>
+                            <div v-if="addTaskForm2.jour1.mercredi.checked == true">
+                                <div>
+                                    <label>mercredi nombre AM </label>
+                                    <input min=0 type="number" v-model="addTaskForm2.jour1.mercredi.nbrAm">
+                                </div>
+
+                                <div>
+                                    <label>mercredi nombre PM </label>
+                                    <input min=0 type="number" v-model="addTaskForm2.jour1.mercredi.nbrPm">
+                                </div>
+                            </div>
+                        </div>
+                        <div style="display:block;">
+                            <input type="checkbox" id="scales" name="scales" v-model="addTaskForm2.jour1.jeudi.checked">
+                            <label for="scales">jeudi</label>
+                            <div v-if="addTaskForm2.jour1.jeudi.checked == true">
+                                <div>
+                                    <label>jeudi nombre AM </label>
+                                    <input min=0 type="number" v-model="addTaskForm2.jour1.jeudi.nbrAm">
+                                </div>
+
+                                <div>
+                                    <label>jeudi nombre PM </label>
+                                    <input min=0 type="number" v-model="addTaskForm2.jour1.jeudi.nbrPm">
+                                </div>
+                            </div>
+                        </div>
+                        <div style="display:block;">
+                            <input type="checkbox" id="scales" name="scales"
+                                v-model="addTaskForm2.jour1.vendredi.checked">
+                            <label for="scales">vendredi</label>
+                            <div v-if="addTaskForm2.jour1.vendredi.checked == true">
+                                <div>
+                                    <label>vendredi nombre AM </label>
+                                    <input min=0 type="number" v-model="addTaskForm2.jour1.vendredi.nbrAm">
+                                </div>
+
+                                <div>
+                                    <label>vendredi nombre PM </label>
+                                    <input min=0 type="number" v-model="addTaskForm2.jour1.vendredi.nbrPm">
+                                </div>
                             </div>
                         </div>
                     </div>
-                    <div style="display:block;">
-                        <input type="checkbox" id="scales" name="scales" v-model="addTaskForm2.jour1.mardi.checked">
-                        <label for="scales">mardi</label>
-                        <div v-if="addTaskForm2.jour1.mardi.checked == true">
-                            <div>
-                                <label>mardi nombre AM </label>
-                                <input min=0 type="number" v-model="addTaskForm2.jour1.mardi.nbrAm">
-                            </div>
 
-                            <div>
-                                <label>mardi nombre PM </label>
-                                <input min=0 type="number" v-model="addTaskForm2.jour1.mardi.nbrPm">
-                            </div>
-                        </div>
-                    </div>
-                    <div style="display:block;">
-                        <input type="checkbox" id="scales" name="scales" v-model="addTaskForm2.jour1.mercredi.checked">
-                        <label for="scales">mercredi</label>
-                        <div v-if="addTaskForm2.jour1.mercredi.checked == true">
-                            <div>
-                                <label>mercredi nombre AM </label>
-                                <input min=0 type="number" v-model="addTaskForm2.jour1.mercredi.nbrAm">
-                            </div>
+                    <div class="col">
+                        <label for="pet-select">Sélectionné les employés à assigné à la taches :</label>
 
-                            <div>
-                                <label>mercredi nombre PM </label>
-                                <input min=0 type="number" v-model="addTaskForm2.jour1.mercredi.nbrPm">
-                            </div>
-                        </div>
-                    </div>
-                    <div style="display:block;">
-                        <input type="checkbox" id="scales" name="scales" v-model="addTaskForm2.jour1.jeudi.checked">
-                        <label for="scales">jeudi</label>
-                        <div v-if="addTaskForm2.jour1.jeudi.checked == true">
-                            <div>
-                                <label>jeudi nombre AM </label>
-                                <input min=0 type="number" v-model="addTaskForm2.jour1.jeudi.nbrAm">
-                            </div>
-
-                            <div>
-                                <label>jeudi nombre PM </label>
-                                <input min=0 type="number" v-model="addTaskForm2.jour1.jeudi.nbrPm">
-                            </div>
-                        </div>
-                    </div>
-                    <div style="display:block;">
-                        <input type="checkbox" id="scales" name="scales" v-model="addTaskForm2.jour1.vendredi.checked">
-                        <label for="scales">vendredi</label>
-                        <div v-if="addTaskForm2.jour1.vendredi.checked == true">
-                            <div>
-                                <label>vendredi nombre AM </label>
-                                <input min=0 type="number" v-model="addTaskForm2.jour1.vendredi.nbrAm">
-                            </div>
-
-                            <div>
-                                <label>vendredi nombre PM </label>
-                                <input min=0 type="number" v-model="addTaskForm2.jour1.vendredi.nbrPm">
-                            </div>
-                        </div>
+                        <select id="pet-select"  multiple size="7" v-model="addTaskForm2.employes">
+                            <option value="tous" >Tout le monde</option>
+                            <option value="nom1">nom1</option>
+                            <option value="nom2">nom2</option>
+                            <option value="nom3">nom3</option>
+                            <option value="nom4">nom4</option>
+                            <option value="nom5">nom5</option>
+                            <option value="nom6">nom6</option>
+                            <option value="nom7">nom7</option>
+                            <option value="nom8">nom8</option>
+                            <option value="nom9">nom9</option>
+                            <option value="nom10">nom10</option>
+                            <option value="nom11">nom11</option>
+                            <option value="nom12">nom12</option>
+                            <option value="nom13">nom13</option>
+                            <option value="nom14">nom14</option>
+                        </select>
                     </div>
                 </div>
-
-                <div class="col">
-                    <label for="pet-select">Sélectionné les employés à assigné à la taches :</label>
-
-                    <select id="pet-select" multiple size="7" v-model="addTaskForm2.employes">
-                        <option value="tous">Tout le monde</option>
-                        <option value="nom1">nom1</option>
-                        <option value="nom2">nom2</option>
-                        <option value="nom3">nom3</option>
-                        <option value="nom4">nom4</option>
-                        <option value="nom5">nom5</option>
-                        <option value="nom6">nom6</option>
-                        <option value="nom7">nom7</option>
-                        <option value="nom8">nom8</option>
-                        <option value="nom9">nom9</option>
-                        <option value="nom10">nom10</option>
-                        <option value="nom11">nom11</option>
-                        <option value="nom12">nom12</option>
-                        <option value="nom13">nom13</option>
-                        <option value="nom14">nom14</option>
-                    </select>
+                <div>
+                    <label>couleur : </label>
+                    <input type="color"  v-model="addTaskForm2.color">
                 </div>
             </div>
-            <div>
-                <label>couleur :  </label>
-                <input type="color" v-model="addTaskForm2.color">
-            </div>
+            <button v-on:click="submitTasks()">submit task</button>
         </div>
-        <button v-on:click="submitTasks()">submit task</button>
-    </div>
+    </from>
 </template>
 <script>
 
